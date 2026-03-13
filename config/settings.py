978e33ee77a0bbd2406e0855135e71ac2ecdc9e6
MasacrePC <L22270526@tuxtla.tecnm.mx>
2026-03-13 14:31:17 -0600

    front

--- config/settings.py
+++ config/settings.py
@@ -131,9 +131,9 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'samuelmesa04@gmail.com'
-EMAIL_HOST_PASSWORD = 'tczq zixh rvuw tljz'
-DEFAULT_FROM_EMAIL = 'samuelmesa04@gmail.com'
+EMAIL_HOST_USER = 'masacregamer23@gmail.com'
+EMAIL_HOST_PASSWORD = 'upfhffpqwpjutqhn'
+DEFAULT_FROM_EMAIL = 'masacregamer23@gmail.com'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/6.0/ref/settings/#default-auto-field
